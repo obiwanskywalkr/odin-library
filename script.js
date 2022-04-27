c4da8fc04c8c5829cec02a6015149ba7b3b86de5
obiwanskywalkr <gp47spd13@gmail.com>
2022-04-27 16:33:39 -0500

    Refactor and add functionality to read status button

--- script.js
+++ script.js
@@ -8,75 +8,87 @@ class Book {
         this.status = status;
         this.index = myLibrary.length;
     }
-
     displayInfo() {
         return (`${this.title} by ${this.author}, ${this.pages} pages, ${this.status}`);
     }
-    changeReadStatus() {
-        
+    toggleReadStatus() {
+        return (this.status ? (this.status = false) : (this.status = true));
     }
 }
 
 function addBookToLibrary() {
-    let title = document.getElementById('title').value;
-    let author = document.getElementById('author').value;
-    let pages = document.getElementById('pages').value;
-    let status = document.getElementById('status').checked;
+    let title = bookForm.title.value;
+    let author = bookForm.author.value;
+    let pages = bookForm.pages.value;
+    let status = bookForm.status.checked;
     
     myLibrary.push(new Book(title, author, pages, status));
 }
 
-async function displayLibrary() {
-    await addBookToLibrary();
+const libraryContainer = document.getElementById('libraryContainer');
 
+function displayLibrary() {
     libraryContainer.innerHTML = '';
 
     for (let i = 0; i < myLibrary.length; i++) {
-        let card = document.createElement('div');
-        card.classList.add('card');
-        card.setAttribute('data-index', `${i}`)
-        libraryContainer.appendChild(card);
-
-        let titleContainer = document.createElement('div');
-        titleContainer.classList.add('cardText');
-        titleContainer.textContent = `"${myLibrary[i].title}"`;
-        card.appendChild(titleContainer);
-
-        let authorContainer = document.createElement('div');
-        authorContainer.classList.add('cardText');
-        authorContainer.textContent = myLibrary[i].author;
-        card.appendChild(authorContainer);
-
-        let pagesContainer = document.createElement('div');
-        pagesContainer.classList.add('cardText');
-        pagesContainer.textContent = `${myLibrary[i].pages} pages`;
-        card.appendChild(pagesContainer);
-
-        let statusContainer = document.createElement('button');
-        statusContainer.classList.add('statusBox');
-        statusContainer.textContent = myLibrary[i].status;
-        card.appendChild(statusContainer);
-
-        let removeBookButton = document.createElement('button');
-        removeBookButton.classList.add('removeBookButton');
-        removeBookButton.textContent = "Remove Book";
-        removeBookButton.addEventListener('click', (e) => {
-            libraryContainer.removeChild(e.currentTarget.parentNode);
-            let currentIndex = e.currentTarget.parentNode.getAttribute('data-index');
-            myLibrary.splice(currentIndex, 1)
-        });
-        card.appendChild(removeBookButton);
+        createBookCard(myLibrary[i]);
     }
 }
 
-const addBookButton = document.getElementById('addBookButton');
-const libraryContainer = document.getElementById('libraryContainer');
+function createBookCard(book) {
+    const card = document.createElement('div');
+    const titleDiv = document.createElement('div');
+    const authorDiv = document.createElement('div');
+    const pagesDiv = document.createElement('div');
+    const statusBtn = document.createElement('button');
+    const removeBookBtn = document.createElement('button');
+
+    card.classList.add('card');
+
+    titleDiv.classList.add('cardText');
+    titleDiv.textContent = `"${book.title}"`;
+    card.appendChild(titleDiv);
+
+    authorDiv.classList.add('cardText');
+    authorDiv.textContent = book.author;
+    card.appendChild(authorDiv);
+
+    pagesDiv.classList.add('cardText');
+    pagesDiv.textContent = `${book.pages} pages`;
+    card.appendChild(pagesDiv);
+
+    statusBtn.classList.add('statusBtn');
+    if (book.status == false) {
+        statusBtn.classList.add('unread');
+        statusBtn.textContent = 'Unread';
+    } else if (book.status == true) {
+        statusBtn.classList.add('read');
+        statusBtn.textContent = 'Read';
+    }
+    card.appendChild(statusBtn);
+   
+    removeBookBtn.classList.add('removeBookBtn');
+    removeBookBtn.textContent = 'Remove Book'
+    card.appendChild(removeBookBtn);
+
+    statusBtn.addEventListener('click', () => {
+        book.toggleReadStatus();
+        displayLibrary();
+    });  
+
+    removeBookBtn.addEventListener('click', () => {
+        myLibrary.splice(myLibrary.indexOf(book), 1);
+        displayLibrary();
+    }); 
+
+    libraryContainer.appendChild(card);
+}
+
+const addBookBtn = document.getElementById('addBookBtn');
 const formOverlay = document.getElementById('formOverlay');
 const formModal = document.getElementById('formModal');
 const bookForm = document.getElementById('bookForm');
-const submitButton = document.getElementById('submitButton');
-const removeBookButton = document.querySelector('.removeBookButton');
-
+const submitBtn = document.getElementById('submitBtn');
 
 function closeForm() {
     formOverlay.classList.remove('displayOverlay');
@@ -89,12 +101,13 @@ function displayForm() {
     formModal.classList.add('displayForm');
 }
 
-addBookButton.addEventListener('click', displayForm);
+addBookBtn.addEventListener('click', displayForm);
 
 formOverlay.addEventListener('click', closeForm);
 
-submitButton.addEventListener('click', (e) => {
+submitBtn.addEventListener('click', (e) => {
     e.preventDefault();
+    addBookToLibrary();
     displayLibrary();
     closeForm();
 });
